f337a5dcd7c0bd201fbbc2c7f4a014694b5f78dc
jjuunnoo <junho9857@gmail.com>
2022-02-24 10:49:54 +0900

    fix 정수 내림차순으로 배치하기

--- 준호/2주차/정수 내림차순으로 배치하기.py
+++ 준호/2주차/정수 내림차순으로 배치하기.py
@@ -6,11 +6,9 @@ Created on Sat Feb 19 16:04:41 2022
 """
 # 프로그래머스
 # 정수 내림차순으로 배치하기
-# 못풀었
 def solution(n):
-    n = list(str(n))
-    n.sort(reverse = True)
-    return int(''.join(n))
+    a = list(str(int(n)))
+    return int(''.join(sorted(a, reverse=True)))
 
 
 
