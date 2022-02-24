ec49f5b159183b066ef2d9d983ad4f0f384f9b6d
plibi <plbr0908@gmail.com>
2022-02-24 17:36:38 +0900

    add

--- 재현/2주차/최대공약수와 최소공배수.py
+++ 재현/2주차/최대공약수와 최소공배수.py
@@ -5,7 +5,7 @@ def solution(n, m):
     # answer = [math.gcd(n, m), n*m/math.gcd(n, m)]
             
     # Euclidean algorithm
-    # 
+    # gcd(a, b) == gcd(b, a%b) 
     a, b = max(n, m), min(n, m)
     while b != 0:
         r = a % b
