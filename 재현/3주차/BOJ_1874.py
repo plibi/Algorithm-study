c1b5e78b8efe0d8a1d6e527bfc1083b38a5c17d8
plibi <plbr0908@gmail.com>
2022-03-01 22:57:40 +0900

    add week3

--- 재현/3주차/BOJ_1874.py
+++ 재현/3주차/BOJ_1874.py
@@ -4,12 +4,20 @@ n = int(input())
 
 answer = []
 seq = [int(input()) for _ in range(n)][::-1]
-
+s = []
+answer = []
 
 for i in range(1, n+1):
-    if i != seq[-1]:
-        print('+')
-    else:
-        print('+')
-        print('-')
-        seq.pop()
+    s.append(i)
+    answer.append('+')
+    if s:
+        while s[-1] == seq[-1]:
+            seq.pop()
+            s.pop()
+            answer.append('-')
+            if not s:
+                break
+if seq:
+    print('NO')
+else:
+    print(*answer, sep='\n')
