4a49b2a594e81e41a84cdbf18e1919ba281756e2
0202hong <khj4661aa@gmail,com>
2022-03-10 03:12:42 +0900

    5th_week(1)

--- 홍진/4th_week/BOJ_2839_설탕 배달.py
+++ 홍진/4th_week/BOJ_2839_설탕 배달.py
@@ -8,9 +8,7 @@ while num >= 0:
         cnt += num // 5  #나누어 떨어진다면 몫을 cnt에 더하고 while문 탈출
         break
     num -= 3
-    if num < 0:          #num 값이 음수가 될 때까지 안된다면 할 수 없는 것이기 때문에 -1을 대입하고 while문 탈출
-        cnt = -1
-        break
     cnt += 1
-
+if num < 0:          #num 값이 음수가 될 때까지 안된다면 할 수 없는 것이기 때문에 -1을 대입하고 while문 탈출
+    cnt = -1
 print(cnt)
