0e06d51e07c8b997514a2f215560036b9dc3651e
plibi <plbr0908@gmail.com>
2022-03-22 23:17:59 +0900

    add week6

--- 재현/6주차/BOJ_1389.py
+++ 재현/6주차/BOJ_1389.py
@@ -1,20 +1,21 @@
 import sys
+from collections import deque
 input = sys.stdin.readline
 n, m = map(int, input().split())
 friends = [[] for _ in range(n+1)]
 
-def dfs(friends, v, end, cnt):
-    print(v, end, cnt)
-    visited[v] = True
-    if end in friends[v]:
-        cnt += 1
-        return cnt
-    else:
-        for i in friends[v]:
+def bfs(friends, v):
+    cnt = [0]*(n+1)
+    queue = deque([v])
+    visited[v] = 1
+    while queue:
+        ver = queue.popleft()
+        for i in friends[ver]:
             if not visited[i]:
-                return dfs(friends, i, end, cnt+1)
-    return cnt
-    
+                queue.append(i)
+                visited[i] = 1
+                cnt[i] = cnt[ver] + 1
+    return sum(cnt)
 
 for _ in range(m):
     f1, f2 = map(int, input().split())
@@ -26,11 +27,7 @@ for i in friends:
 
 count = []
 for i in range(1, n+1):
-    cnt = 0
-    for j in range(1, n+1):
-        if i != j:
-            visited = [False]*(n+1)
-            # print(dfs(friends, i, j, 0))
-            cnt += dfs(friends, i, j, 0)
-    count.append(cnt)
-print(count)
+    visited = [0]*(n+1)
+    count.append(bfs(friends, i))
+print(count.index(min(count)) + 1)
+
