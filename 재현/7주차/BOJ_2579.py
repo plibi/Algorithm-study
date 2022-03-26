d3c0fc53b9bcf0fec5b35f61cc32694b7073008c
plibi <plbr0908@gmail.com>
2022-03-26 23:54:48 +0900

    fix week7

--- 재현/7주차/BOJ_2579.py
+++ 재현/7주차/BOJ_2579.py
@@ -17,3 +17,13 @@ else:
         table[i] = max(stairs[i]+stairs[i-1]+table[i-3], stairs[i]+table[i-2])
 
     print(table[-1])
+
+
+# 맨 마지막 계단부터 거꾸로 생각
+# 마지막 n번째 계단
+# 1. n-1, n-2 중에 큰 값을 가진 계단을 선택
+# 1-1. n-1이라면, 이전계단은 무조건 n-3. (n-2는 연속된 3계단)
+# 1-2. n-2라면, n-3과 n-3 중 큰 값을 가진 계단. (n-2를 기준으로 1번 반복)
+# 2. 다시반복
+# table은 점수의 합
+# => table[i] = max(table[i-1]+stairs[n]+stairs[n-3], table[i-2]+stairs[n])
